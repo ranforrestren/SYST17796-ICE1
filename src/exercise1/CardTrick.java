06ed6963bf02be17f0476cdfe20620241615d8e6
Ran Ren <ranforrestren@gmail.com>
2023-01-27 13:41:39 -0500

    Generate Random Hand

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,5 @@
 package exercise1;
-
+import java.util.*;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -16,13 +16,18 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
+            Random ran = new Random();
+            int random = ran.nextInt(7) + 1;
+            card.setValue(random);
+            random = ran.nextInt(4);
+            card. setSuit(Card.SUITS[random]);
+            System.out.println(card.getValue());
+            System.out.println(card.getSuit());
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
-
+        
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
