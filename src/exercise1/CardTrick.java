82f97b9cae9dcb81e71bcfc571ff6c6d52aeeb56
Ran Ren <ranforrestren@gmail.com>
2023-02-03 11:32:09 -0500

    I'm Done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -60,7 +60,7 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
-    
+        // I'm Done!
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
